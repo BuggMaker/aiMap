ec4ba38c4a7cdd0eb416caa4da1ebd8b5a601c19
weicai_wang <898906871@qq.com>
2018-02-03 11:12:44 +0800

    基本实现Bound类

--- src/geometry/Bound.js
+++ src/geometry/Bound.js
@@ -5,44 +5,144 @@ import {
     Size
 } from "../core/Size";
 import {
-    Extent
-} from "../core/Extent";
-import {
-    UtilObj
+    UtilObj,
+    isArray
 } from "../index";
-import { Point } from "./vector/Point";
+import {
+    Point
+} from "./vector/Point";
 
 /**
- * 边界(外接矩形框)
+ * 边界
+ * 一般指世界坐标系的图形对象的外接矩形
+ * @param {Point,Point || Array} pt1,pt2 || [[x1,y1],[x2,y2]]
  */
-var IBound = Class.extend({
-    name: 'IBound',
+var Bound = Class.extend({
+    name: 'Bound',
     constructor: function (...args) {
-        this.extent = new Extent()
+        // 范围
+        this.min = new Point(Number.MAX_VALUE, Number.MAX_VALUE)
+        this.max = new Point(Number.MIN_VALUE, Number.MIN_VALUE)
+        //根据参数 重新计算范围
+        if (args.length == 1 && isArray(args[0]) && args[0].length >= 2) {
+            var ary = args[0]
+            for (let index = 0; index < ary.length; index++) {
+                var pt = new Point(ary[index][0], ary[index][1])
+                this.min.x = Math.min(pt.x, this.min.x)
+                this.min.y = Math.min(pt.y, this.min.y)
+                this.max.x = Math.max(pt.x, this.max.x)
+                this.max.y = Math.max(pt.y, this.max.y)
+            }
+        } else if (args.length == 2) {
+            var pt1 = args[0],
+                pt2 = args[1]
+            if (pt1.isInstanceOf(Point) && pt2.isInstanceOf(Point)) {
+                this.min.x = Math.min(pt1.x, pt2.x)
+                this.min.y = Math.min(pt1.y, pt2.y)
+                this.max.x = Math.max(pt1.x, pt2.x)
+                this.max.y = Math.max(pt1.y, pt2.y)
+            }
+        }
+        // 尺寸
         UtilObj.defineProp(this, 'size', {
             get: () => {
-                return new Size(this.extent.maxX - this.extent.minX, this.extent.maxY - this.extent.minY)
+                return new Size(this.max.x - this.min.x, this.max.y - this.min.y)
             }
         })
-
+        // 中心
         UtilObj.defineProp(this, 'center', {
             get: () => {
-                 return new Point(this.extent.maxX - this.extent.minX, this.extent.maxY - this.extent.minY)
+                return new Point(this.min.x + this.size.width / 2, this.min.y + this.size.height / 2)
             }
         })
     },
     publics: {
-        // 判断是否相较
+        /**
+         * 判断是否相交
+         * @param {Bound} bound
+         * @return {Boolean}
+         */
         intersectWith: function (bound) {
-            throw `function 'intersect' of ${this.name} is unrealized!`
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var tarMin = bound.min,
+                tarMax = bound.max
+            var intersectX = (tarMax.x >= this.min.x) && (tarMin.x <= this.max.x),
+                intersectY = (tarMax.y >= this.min.y) && (tarMin.y <= this.max.y)
+            return intersectX && intersectY
         },
-        // 取交集
+        /**
+         * 取交集
+         * @param {Bound} bound
+         * @return {Bound} 新的Bound对象
+         */
         intersection: function (bound) {
-            throw `function 'intersection' of ${this.name} is unrealized!`
+            // throw `function 'intersect' of ${this.name} is unrealized!`
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var result = new Bound()
+            var tarMin = bound.min,
+                tarMax = bound.max
+            result.min.x = Math.max(tarMin.x, this.min.x)
+            result.min.y = Math.max(tarMin.y, this.min.y)
+            result.max.x = Math.min(tarMax.x, this.max.x)
+            result.max.x = Math.min(tarMax.y, this.max.y)
+            return result
         },
-        // 取并集
+        /**
+         * 取并集 
+         * @param {Bound} bound
+         * @return {Bound} 返回新的Bound对象
+         */
         union: function (bound) {
-            throw `function 'union' of ${this.name} is unrealized!`
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var result = new Bound()
+            var tarMin = bound.min,
+                tarMax = bound.max
+            result.min.x = Math.min(tarMin.x, this.min.x)
+            result.min.y = Math.min(tarMin.y, this.min.y)
+            result.max.x = Math.max(tarMax.x, this.max.x)
+            result.max.x = Math.max(tarMax.y, this.max.y)
+            return result
+        },
+        /**
+         * 扩充当前范围
+         * @param {Bound} bound
+         * @return {this}
+         */
+        extend: function (bound) {
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var tarMin = bound.min,
+                tarMax = bound.max
+            this.min.x = Math.min(tarMin.x, this.min.x)
+            this.min.y = Math.min(tarMin.y, this.min.y)
+            this.max.x = Math.max(tarMax.x, this.max.x)
+            this.max.x = Math.max(tarMax.y, this.max.y)
+
+            return this
+        },
+        /**
+         * 判断是否存在重叠区域
+         * @param {Bound} bound
+         * @return {Boolean} 
+         */
+        overlapWith: function (bound) {
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var tarMin = bound.min,
+                tarMax = bound.max
+            var overlapX = (tarMax.x > this.min.x) && (tarMin.x < this.max.x),
+                overlapY = (tarMax.y > this.min.y) && (tarMin.y < this.max.y)
+            return overlapX && overlapY
+        },
+        /**
+         * 判断是否完全覆盖目标范围,cover
+         * @param {Bound} bound
+         */
+        coverWidth: function (bound) {
+            if (!bound.isInstanceOf(Bound)) throw 'param is not instance of Bound'
+            var tarMin = bound.min,
+                tarMax = bound.max
+            var overlapX = (tarMax.x <= this.max.x) && (tarMin.x >= this.min.x),
+                overlapY = (tarMax.y <= this.max.y) && (tarMin.y >= this.min.y)
+            return overlapX && overlapY
         }
     }
 })
